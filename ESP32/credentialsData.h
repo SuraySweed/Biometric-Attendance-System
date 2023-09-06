1ec40d70c16a37e1fef56ffac850fef9d3b8d151
suray sweed <suray263@gmail.com>
2023-09-06 09:58:59 +0300

    adding keypad test to ESP folder

--- ESP32/credentialsData.h
+++
@@ -1,9 +0,0 @@
-//************************************************************************
-/* Set these to your desired credentials. */
-//const char *ssid = "HOTBOX 4-4E78-5GHz"; //ENTER YOUR WIFI SETTINGS
-//const char *password = "DV29QJWG3KN9";
-const char *ssid = "Suray Sweed "; //ENTER YOUR WIFI SETTINGS
-const char *password = "12345678";
-//const char *ssid = "Boran"; //ENTER YOUR WIFI SETTINGS
-//const char *password = "zeuv8379";
-String link_s = "http://172.20.10.2/biometricattendance/getdata.php"; //computer IP or the server domain
